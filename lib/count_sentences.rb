96a38b5be1019f8cb0eb6bee4fb01b937a9e087e
akramhelil <akramhelil@gmail.com>
2019-01-31 17:10:12 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,8 +27,10 @@ class String
   end
 
   def count_sentences
-    self.split(/[?!.]+/).count  
+    self.split(/[?!.]+/).count
 
+#searched for google how to count sentence in ruby, got the answer to the (/[?!.]+/)
+# What is the diffrence between, (/[?!.]+/), (/[?!.]/) and (/[?!.]-/)  ??
   end
 
 end  # String Class ends here
